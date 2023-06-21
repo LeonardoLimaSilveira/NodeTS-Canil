53c956a263e579b06e6f559de25eadf1f8d243ee
leonardo <leolimaa@proton.me>
2023-06-21 05:45:29 -0300

    final commit

--- src/controllers/page.controller.ts
+++ src/controllers/page.controller.ts
@@ -1,39 +1,50 @@
 import { Request, Response } from 'express'
 import { createMenuObject } from '../helpers/createMenuObject'
+import { Pet } from '../models/pet'
 
 export const home = (req: Request, res: Response) => {
+  let list = Pet.getAll()
   res.render('pages/page', {
     banner: {
       menu: createMenuObject('all'),
       title: 'Todos os animais',
       background: 'allanimals.jpg'
-    }
+    },
+    list
   })
 }
 export const dogs = (req: Request, res: Response) => {
+  let list = Pet.getFromType('dog')
   res.render('pages/page', {
     banner: {
       menu: createMenuObject('dog'),
       title: 'Cachorros',
       background: 'banner_dog.jpg'
-    }
+    },
+    list
   })
 }
 export const cats = (req: Request, res: Response) => {
+  let list = Pet.getFromType('cat')
+
   res.render('pages/page', {
     banner: {
       menu: createMenuObject('cat'),
       title: 'Gatos',
       background: 'banner_cat.jpg'
-    }
+    },
+    list
   })
 }
 export const fish = (req: Request, res: Response) => {
+  let list = Pet.getFromType('fish')
+
   res.render('pages/page', {
     banner: {
       menu: createMenuObject('fish'),
       title: 'Peixes',
       background: 'banner_fish.jpg'
-    }
+    },
+    list
   })
 }
